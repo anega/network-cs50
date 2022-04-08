e89ba77c2e73eaaebdfbc22903a141508b83bd85
anega <anegamega@gmail.com>
2022-04-08 15:41:33 +0300

    Customize Post view in admin

--- network/admin.py
+++ network/admin.py
@@ -2,5 +2,10 @@ from django.contrib import admin
 
 from network.models import User, Post
 
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("body", "author", "created_at")
+
+
 admin.site.register(User)
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
