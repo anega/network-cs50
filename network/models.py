07d45d78aaf75d680b63e4ef5b03f25826bae82b
anega <anegamega@gmail.com>
2022-04-08 16:06:13 +0300

    Edit Post model(remove unnecessary fields attributes)

--- network/models.py
+++ network/models.py
@@ -9,9 +9,9 @@ class User(AbstractUser):
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, null=False, related_name="posts")
     body = models.TextField(blank=False, null=False)
-    created_at = models.DateTimeField(auto_now_add=True, blank=False, null=False)
-    last_edit = models.DateTimeField(auto_now=True, blank=False, null=False)
-    like = models.ManyToManyField(User, related_name="posts_liked")
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_edit = models.DateTimeField(auto_now=True)
+    like = models.ManyToManyField(User, blank=True, null=True, related_name="posts_liked")
 
     def __str__(self):
         return f"{self.body[0:15]} {self.author} {self.created_at}"
