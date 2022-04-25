4400749d6e5b1441af06b53b3a1de0e43cd4d52a
anega <anegamega@gmail.com>
2022-04-25 12:48:13 +0300

    Check if follow btn exists on the page

--- network/static/network/scripts.js
+++ network/static/network/scripts.js
@@ -1,57 +1,59 @@
 document.addEventListener('DOMContentLoaded', () => {
     const follow_btn = document.getElementById('follow-btn')
-    follow_btn.addEventListener('click', (event) => {
-        event.preventDefault()
-        const csrfToken = getCookie('csrftoken')
-        const targetLink = event.target
-        const url = targetLink.href
-        const userId = targetLink.dataset.id
-        const followAction = targetLink.dataset.action
-        fetch(url, {
-            method: 'POST',
-            headers: {
-                'X-CSRFToken': csrfToken,
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                id: userId,
-                action: followAction
+    if (follow_btn) {
+        follow_btn.addEventListener('click', (event) => {
+            event.preventDefault()
+            const csrfToken = getCookie('csrftoken')
+            const targetLink = event.target
+            const url = targetLink.href
+            const userId = targetLink.dataset.id
+            const followAction = targetLink.dataset.action
+            fetch(url, {
+                method: 'POST',
+                headers: {
+                    'X-CSRFToken': csrfToken,
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    id: userId,
+                    action: followAction
+                })
             })
-        })
-            .then(response => {
-                if (response.ok) return response.json()
-                throw new Error()
-            })
-            .then(data => {
-                console.log(data)
-                if (data['status'] === 'ok') {
-                    targetLink.dataset.action = followAction === 'follow' ? 'unfollow' : 'follow'
-                    targetLink.innerHTML = followAction === 'follow' ? 'Unfollow' : 'Follow'
-                    const prevFollowers = document.getElementsByClassName('followers-count')[0]
-                    let prevFollowersCount = parseInt(prevFollowers.innerHTML)
-                    if (followAction === 'follow') {
-                        prevFollowersCount += 1
-                        if (prevFollowersCount === 1) {
-                            prevFollowers.innerHTML = `${prevFollowersCount} follower`
-                        } else {
-                            prevFollowers.innerHTML = `${prevFollowersCount} followers`
-                        }
-                    } else {
-                        prevFollowersCount -= 1
-                        if (prevFollowersCount === 1) {
-                            prevFollowers.innerHTML = `${prevFollowersCount} follower`
-                        } else {
-                            prevFollowers.innerHTML = `${prevFollowersCount} followers`
-                        }
-                    }
-                } else if (data['status'] === 'error') {
+                .then(response => {
+                    if (response.ok) return response.json()
                     throw new Error()
-                }
-            })
-            .catch(error => {
-                console.log('Something went wrong. Please try again later.')
-            })
-    })
+                })
+                .then(data => {
+                    console.log(data)
+                    if (data['status'] === 'ok') {
+                        targetLink.dataset.action = followAction === 'follow' ? 'unfollow' : 'follow'
+                        targetLink.innerHTML = followAction === 'follow' ? 'Unfollow' : 'Follow'
+                        const prevFollowers = document.getElementsByClassName('followers-count')[0]
+                        let prevFollowersCount = parseInt(prevFollowers.innerHTML)
+                        if (followAction === 'follow') {
+                            prevFollowersCount += 1
+                            if (prevFollowersCount === 1) {
+                                prevFollowers.innerHTML = `${prevFollowersCount} follower`
+                            } else {
+                                prevFollowers.innerHTML = `${prevFollowersCount} followers`
+                            }
+                        } else {
+                            prevFollowersCount -= 1
+                            if (prevFollowersCount === 1) {
+                                prevFollowers.innerHTML = `${prevFollowersCount} follower`
+                            } else {
+                                prevFollowers.innerHTML = `${prevFollowersCount} followers`
+                            }
+                        }
+                    } else if (data['status'] === 'error') {
+                        throw new Error()
+                    }
+                })
+                .catch(error => {
+                    console.log('Something went wrong. Please try again later.')
+                })
+        })
+    }
 })
 
 function getCookie(name) {
